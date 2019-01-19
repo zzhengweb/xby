a2c20c5a5c9daf8a82bf0a9bd0be9f66f35e5b24
zzheng <434542284@qq.com>
2019-01-19 12:47:53 +0800

    new

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,41 +1,3 @@
-<<<<<<< HEAD
-import React,{ Component } from 'react';
-import { Row, Col } from 'antd';
-import {
-	HeaderWrapper,
-	ImgLogo,
-	HeaderNav
-} from './style';
-import logoUrl from './images/logo.png';
-import { Link } from 'react-router-dom';
-
-class Header extends Component {
-	render(){
-		return(
-			<HeaderWrapper>
-				<Row>
-					<Col span={12}>
-						<ImgLogo alt="logo" src={logoUrl} />
-					</Col>
-					<Col span={12}>
-						<HeaderNav>
-							<li><Link to="/">首页</Link ></li>
-							<li><Link to="flow">定制流程</Link ></li>
-							<li><Link to="evaluation">顾客评价</Link ></li>
-							<li><Link to="about">关于CHINESPAIN</Link ></li>
-							<li><Link to="consultant">旅行攻略</Link ></li>
-							<li><Link to="custom">联系我们</Link ></li>
-						</HeaderNav>
-					</Col>
-				</Row>
-			</HeaderWrapper>
-		)
-	}
-}
-
-export default Header;
-||||||| merged common ancestors
-=======
 import React,{ Component } from 'react';
 import { Row, Col } from 'antd';
 import {
@@ -71,4 +33,3 @@ class Header extends Component {
 }
 
 export default Header;
->>>>>>> origin/master
